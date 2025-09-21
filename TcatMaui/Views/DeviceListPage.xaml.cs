41d76cb1412f720f0206d680b9224c0533f628f4
Arnulf <a.rupp@inventronicsglobal.com>
2025-09-21 11:36:37 +0200

    Fix issue wiht advertising on iOS where BLE flags record is not visble to the application

--- TcatMaui/Views/DeviceListPage.xaml.cs
+++ TcatMaui/Views/DeviceListPage.xaml.cs
@@ -56,15 +56,26 @@ public partial class DeviceListPage : ContentPage
         if (e.Device == null) return;    // List only Bluetooth devices
         if (e.Device.Rssi < minRssi) return;
         //if (String.IsNullOrEmpty(e.Name)) return;
-        if (e.Device.AdvertisementRecords.Count() < 2) return;
-        if (e.Device.AdvertisementRecords[0].Data.Count() < 1) return;
-        if (e.Device.AdvertisementRecords[1].Data.Count() < 2) return;
 
-        if (e.Device.AdvertisementRecords[0].Type != Plugin.BLE.Abstractions.AdvertisementRecordType.Flags) return;
-        if (e.Device.AdvertisementRecords[0].Data[0] != 6) return;
-        if (e.Device.AdvertisementRecords[1].Type != Plugin.BLE.Abstractions.AdvertisementRecordType.ServiceData) return;
-        if (e.Device.AdvertisementRecords[1].Data[0] != 251) return;
-        if (e.Device.AdvertisementRecords[1].Data[1] != 255) return;
+        if (e.Device.AdvertisementRecords.Count() >= 2)
+        {
+            if (e.Device.AdvertisementRecords[0].Data.Count() < 1) return;
+            if (e.Device.AdvertisementRecords[1].Data.Count() < 2) return;
+
+            if (e.Device.AdvertisementRecords[0].Type != Plugin.BLE.Abstractions.AdvertisementRecordType.Flags) return;
+            if (e.Device.AdvertisementRecords[0].Data[0] != 6) return;
+            if (e.Device.AdvertisementRecords[1].Type != Plugin.BLE.Abstractions.AdvertisementRecordType.ServiceData) return;
+            if (e.Device.AdvertisementRecords[1].Data[0] != 251) return;
+            if (e.Device.AdvertisementRecords[1].Data[1] != 255) return;
+        }
+        else if (e.Device.AdvertisementRecords.Count() == 1)
+        {
+            if (e.Device.AdvertisementRecords[0].Data.Count() < 2) return;
+            if (e.Device.AdvertisementRecords[0].Type != Plugin.BLE.Abstractions.AdvertisementRecordType.ServiceData) return;
+            if (e.Device.AdvertisementRecords[0].Data[0] != 251) return;
+            if (e.Device.AdvertisementRecords[0].Data[1] != 255) return;
+        }
+        else return;
 
         //string device_name = e.Device.AdvertisementRecords[0].Type.ToString() + ":" + e.Device.AdvertisementRecords[0].Data[0] + ", " + e.Device.AdvertisementRecords[1].Type.ToString() + ":" + e.Device.AdvertisementRecords[1].Data[0] + "," + e.Device.AdvertisementRecords[1].Data[1];
         string device_name = String.IsNullOrEmpty(e.Device.Name) ? "<no name: " + e.Device.Id.ToString() + ">" : e.Device.Name + " (" + e.Device.Id.ToString() + ")";
